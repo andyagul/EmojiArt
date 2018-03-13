8e43a7012e9a5b97d40db15fcc1b190c20e14b24
Chen Weiru <506624047@qq.com>
2018-03-13 22:26:37 +0800

    Finishing 50mins of lecture 12

--- EmojiArt/EmojiViewController.swift
+++ EmojiArt/EmojiViewController.swift
@@ -11,7 +11,20 @@ import UIKit
 class EmojiViewController: UIViewController,UIDropInteractionDelegate,UIScrollViewDelegate,UICollectionViewDelegate,UICollectionViewDataSource,UICollectionViewDelegateFlowLayout, UICollectionViewDragDelegate, UICollectionViewDropDelegate  {
    
     func collectionView(_ collectionView: UICollectionView, performDropWith coordinator: UICollectionViewDropCoordinator) {
-        
+        let destinationIndexPath = coordinator.destinationIndexPath ?? IndexPath(item:0, section:0)
+        for item in coordinator.items{
+            if let sourceIndexPath = item.sourceIndexPath{
+                if let attributedString = item.dragItem.localObject as? NSAttributedString{
+                    collectionView.performBatchUpdates({
+                        emojis.remove(at: sourceIndexPath.item)
+                        emojis.insert(attributedString.string, at:destinationIndexPath.item)
+                        collectionView.deleteItems(at: [sourceIndexPath])
+                        collectionView.insertItems(at: [destinationIndexPath])
+                    }, completion: nil)
+                    coordinator.drop(item.dragItem, toItemAt: destinationIndexPath)
+                }
+            }
+        }
     }
     
     func collectionView(_ collectionView: UICollectionView, canHandle session: UIDropSession) -> Bool {
